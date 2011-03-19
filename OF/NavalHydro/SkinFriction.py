c99ad03ff748570a11622d475f9b4a092fb74128
Jens Hoepken <jhoepken@gmail.com>
2011-03-19 15:45:50 +0100

    Added force coefficient function

--- OF/NavalHydro/SkinFriction.py
+++ OF/NavalHydro/SkinFriction.py
@@ -7,12 +7,48 @@ architecture. All definitions should be based on the Reynoldsnumber as well as
 on the velocity and the Froude number.
 """
 
-def cd(F,
+def forceCoeff(F,
         Swett,
         v=None,
         Re=None,
-        Fr=None):
-    pass
+        Fr=None,
+        **kwargs):
+    """
+    Calculates the force coefficient by
+
+    ..math::
+
+        C = \\frac{F}{0.5\\rho S_{wett} v^2}
+
+    :param F: Force
+    :type F: float
+    :param Swett: Wetted surface area
+    :type Swett: float
+    :param v: Velocity
+    :type v: float
+    :param Re: Reynoldsnumber
+    :type Re: float
+    :param Fr: Froudenumber
+    :type Fr: float
+
+    :rtype: float
+
+    :Author: Jens Hoepken <jhoepken@gmail.com>
+    """
+
+
+    if not 'rho' in kwargs.iterkeys():
+        rho = Constants.water['rho']
+    else:
+        rho = kwargs['rho']
+
+    if Re:
+        v = FlowProperties.Re(Re=Re,L=kwargs['L'])
+    elif Fr:
+        v = FlowProperties.Fr(Fr=Fr,L=kwargs['L'])
+
+
+    return F/(0.5*rho*Swett*v**2)
 
 
 def ittc57(Re=None,
