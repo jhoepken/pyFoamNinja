3ecdce5bf7a24558c4f1ae4342d70f8713739c1e
Jens Hoepken <jhoepken@gmail.com>
2011-03-20 22:11:51 +0100

    Implemented case handling

---
+++ OF/Basic/Case.py
@@ -0,0 +1,63 @@
+from os import listdir
+from os.path import join
+
+from PyFoam.RunDictionary.SolutionDirectory import SolutionDirectory
+
+from OF import Settings
+
+class case(SolutionDirectory):
+    """
+    Inherits all function from
+    :class:`PyFoam.RunDictionary.SolutionDirectory` and reads some
+    properties from the case on initialisation.
+
+    :Author: Jens Hoepken <jhoepken@gmail.com>
+    """
+
+    turbulenceModel = None
+    """
+    Stores the name of the employed turbulence model
+
+    :type: string
+    """
+
+    inletPatch = None
+    """
+    Stores the name of the inlet patch
+
+    :type: string
+    """
+
+    def __init__(
+                    self,
+                    name,
+                    archive=None,
+                    paraviewLink=True,
+                    parallel=False,
+                    region=None,
+                    **kwargs
+                ):
+        """
+        :param name: Name of the solution directory
+        :type name: string
+        """
+        # Run the parent constructor
+        SolutionDirectory.__init__(
+                                    self,
+                                    name,
+                                    archive=archive,
+                                    paraviewLink=paraviewLink,
+                                    parallel=parallel,
+                                    region=region
+                                    )
+
+        # Get the used turbulence model
+        self.turbulenceModel = self.getDictionaryContents(
+                                                self.constantDir(),
+                                                'RASProperties'
+                                            )['RASModel']
+
+        if 'inletPatch' in kwargs.iterkeys():
+            self.inletPatch = kwargs['inletPatch']
+        else:
+            self.inletPatch = Settings.inletPatch
