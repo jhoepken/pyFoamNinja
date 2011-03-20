917b7a027fa50a786118073c4f99521d05eb3ab4
Jens Hoepken <jhoepken@gmail.com>
2011-03-20 22:09:39 +0100

    Changed the FlowProperties handling to a more convenient manner

--- OF/Basic/FlowProperties.py
+++ OF/Basic/FlowProperties.py
@@ -1,11 +1,18 @@
 from math import sqrt
 from OF import Constants
 
+"""
+This package contains some descriptive flow parameters, that are used regularly.
+"""
+
 def Re(**kwargs):
     """
     Calculates the Reynoldsnumber given by
 
-    :math:`Re = \\frac{v*S_{wett}}{\\nu}`
+    ..math::
+        
+        Re = \\frac{v*L}{\\nu}
+
 
     If the velocity is given, the Reynoldsnumber is returned and if a
     Froudenumber is provided as an argument, the Reynoldsnumber is calculated
@@ -34,10 +41,7 @@ def Re(**kwargs):
         return kwargs['v']*kwargs['L']/nu
 
     elif 'Fr' in kwargs.iterkeys():
-        return Fr(
-                    Fr=kwargs['Fr'],
-                    L=kwargs['L']
-                 )
+        return Fr(**kwargs)
 
     elif 'Re' in kwargs.iterkeys():
         return kwargs['Re']*nu/kwargs['L']
@@ -47,7 +51,9 @@ def Fr(**kwargs):
     """
     Calculates the Froudenumber given by
 
-    :math:`Re = \\frac{v*}{\sqrt{gL}}`
+    ..math::
+
+        Re = \\frac{v}{\sqrt{gL}}
 
     If the velocity is given, the Froudenumber is returned and if a
     Reynoldsnumber is provided as an argument, the Froudenumber is calculated
@@ -71,11 +77,48 @@ def Fr(**kwargs):
         return kwargs['v']/sqrt(Constants.g*kwargs['L'])
 
     elif 'Re' in kwargs.iterkeys():
-        vRe =  Re(
-                    Re=kwargs['Re'],
-                    L=kwargs['L']
-                )
-        return Fr(v=vRe,L=kwargs['L'])
+        kwargs['v'] =  Re(**kwargs)
+        return Fr(**kwargs)
 
     elif 'Fr' in kwargs.iterkeys():
         return kwargs['Fr']*sqrt(Constants.g*kwargs['L'])
+
+def vFrRe(**kwargs):
+    """
+    Calculates the velocity, Reynoldsnumber and Froudenumber, based on one
+    velocity description and a reference length. All three are returned.
+
+    :rtype: float,float,float
+    """
+    velocityParams = ['v','Re','Fr']
+
+    pCount = 0
+    for vI in velocityParams:
+        if vI in kwargs.iterkeys():
+            pCount += 1
+    
+
+    if not kwargs['v'] and not kwargs['Re'] and not kwargs['Fr']:
+        raise ValueError("Neither v nor Re and Fr have been provided")
+    elif pCount > 1 :
+        raise ValueError("Too many velocity definitions (Re, v, Fr)")
+
+    if not 'L' in kwargs.iterkeys():
+        raise ValueError("A reference length has to be specified")
+
+    if kwargs['v']:
+        v_ = kwargs['v']
+        Fr_ = Fr(**kwargs)
+        Re_ = Re(**kwargs)
+
+    elif kwargs['Fr']:
+        v_ = Fr(**kwargs)
+        Fr_ = kwargs['Fr']
+        Re_ = Re(**kwargs)
+
+    elif kwargs['Re']:
+        v_ = Re(**kwargs)
+        Fr_ = Fr(**kwargs)
+        Re_ = kwargs['Re']
+
+    return v_,Fr_,Re_
