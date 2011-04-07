37d8c72ccc9949cf4ce8bf9c8e928db01f702470
Jens Hoepken <jhoepken@gmail.com>
2011-04-07 15:47:35 +0200

    Changed indentation

--- OF/PostProcessing/DataFile.py
+++ OF/PostProcessing/DataFile.py
@@ -18,7 +18,6 @@ class dataFile():
     """
 
     _fileData = None
-
     """
     Content of the file, read via :meth:`readlines()`
 
@@ -35,42 +34,42 @@ class dataFile():
 
         # Read the data file via readlines and store the content
         # tmpFile = open(path,"r")
-	# No need for explicit garbage collection, automatic variable dies
-	# anyway and the handle is given to _fileData: this is faster, I 
-	# have tested it out. 
+        # No need for explicit garbage collection, automatic variable dies
+        # anyway and the handle is given to _fileData: this is faster, I 
+        # have tested it out. 
         self._fileData = open(path,"r").readlines()
         #tmpFile.close()
         # del tmpFile
         self.parse()
 
     def __getitem__(self,key):
-	# Get the column data from the _fileData array.
-	return self._fileData[:,key]
+        # Get the column data from the _fileData array.
+        return self._fileData[:,key]
 
     def parse(self):
         """
         Uses regular expressions to extract a list of numbers from the lines in
-	the file. Skips all lines that start with a comment symbol.
+        the file. Skips all lines that start with a comment symbol.
         """
-	parsedData = []
+        parsedData = []
 
         # Regular expression for a number: integer, float, double with support
-	# for scientific notation.
-	numberRe = re.compile(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?")
-	# Possible line comments.
+        # for scientific notation.
+        numberRe = re.compile(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?")
+        # Possible line comments.
         lineComments = ("//", "%","#") 
 
-	for line in self._fileData:
-	    # Remove the whitespace on the left of the line.
-	    line = line.lstrip()
-	    if not line.startswith(lineComments):
-		# re.findall returns a list of strings that are to be mapped
-		# into float values by the "map" builtin function.
-		# The result will be a list of numbers that are added to the
-		# data list.
-		parsedData.append(map(lambda number: float(number),
-		                  re.findall(numberRe,line)));
+        for line in self._fileData:
+            # Remove the whitespace on the left of the line.
+            line = line.lstrip()
+            if not line.startswith(lineComments):
+                # re.findall returns a list of strings that are to be mapped
+                # into float values by the "map" builtin function.
+                # The result will be a list of numbers that are added to the
+                # data list.
+                parsedData.append(map(lambda number: float(number),
+                    re.findall(numberRe,line)));
 
         # Overwrite the file raw ASCII data with the numpy.array object that
-	# is a result of the data mining. 
+        # is a result of the data mining. 
         self._fileData = numpy.array(parsedData)
